5a6991fe65849bca81cb9d8e84a547a3a6ea493d
frey <1007921963@qq.com>
2024-06-01 17:06:43 +0800

    P9 Debug done with camera set

--- Assets/Scripts/Fight/FightInit.cs
+++ Assets/Scripts/Fight/FightInit.cs
@@ -11,6 +11,9 @@ public class FightInit : FightUnit
         AudioManager.Instance.PlayBGM("battle");
         //显示战斗界面
         UIManager.Instance.ShowUI<FightUI>("FightUI");
+
+        //敌人生成
+        EnemyManager.Instance.LoadRes("10003");//此处读取关卡3的敌人信息
     }
 
     public override void OnUpdate()
---
+++ Assets/Scripts/Manager/EnemyManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager
+{
+    public static EnemyManager Instance = new EnemyManager();
+
+    private List<Enemy> enemyList;
+    public void LoadRes(string id)
+    {
+        enemyList = new List<Enemy>();
+        Dictionary<string, string> LevelData = GameConfigManager.Instance.GetLevelById(id);
+        
+        string[] enemyIds = LevelData["EnemyIds"].Split('=');
+        string[] enemyPos = LevelData["Pos"].Split('=');
+        
+        for(int i = 0; i < enemyIds.Length; i++)
+        {
+            string enemyId = enemyIds[i];
+            string[] posArr = enemyPos[i].Split(',');
+            float x = float.Parse(posArr[0]);
+            float y = float.Parse(posArr[1]);
+            float z = float.Parse(posArr[2]);
+            Dictionary<string, string> enemyData = GameConfigManager.Instance.GetEnemyById(enemyId);
+
+            GameObject prefab = Resources.Load<GameObject>(enemyData["Model"]);
+            GameObject obj = Object.Instantiate(prefab, new Vector3(x, y, z), prefab.transform.rotation);
+            Enemy enemy = obj.AddComponent<Enemy>();
+            enemy.Init(enemyData);
+        }
+    }
+}
